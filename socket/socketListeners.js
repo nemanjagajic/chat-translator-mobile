b243daac15505077ce230a9a4e0a1143fb9cce40
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-05-10 16:44:22 +0200

    implemented proper chat messaging refresh

--- socket/socketListeners.js
+++ socket/socketListeners.js
@@ -1,10 +1,18 @@
 import socket from './index'
 import store from '../store'
-import {appendMessageAndCropLimit} from '../store/chats/actions'
+import {appendMessageAndCropLimit, getChats} from '../store/chats/actions'
 import {MESSAGES_PAGINATION_LIMIT} from '../constants/Messages'
 
 socket.on('loadMessage', message => {
-  store.dispatch(appendMessageAndCropLimit({ paginationLimit: MESSAGES_PAGINATION_LIMIT, message }))
+  const state = store.getState()
+  const openedChat = state.chats.openedChat
+  if (!openedChat) {
+    store.dispatch(getChats({ showLoadingIndicator: false }))
+    return
+  }
+  if (openedChat._id === message.chatId) {
+    store.dispatch(appendMessageAndCropLimit({paginationLimit: MESSAGES_PAGINATION_LIMIT, message}))
+  }
 })
 
 export default {}
