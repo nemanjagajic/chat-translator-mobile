c4ea35c2673f22a7bf50c208545fae874a9ae626
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-05-10 16:22:28 +0200

    creating socket session for user implemented

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,17 @@
 import io from 'socket.io-client'
 import config from '../config'
+import {AsyncStorage} from 'react-native'
 
 const socket = io(config.APP_URL, {
   transports: ['websocket'],
+})
+
+socket.on('connect', async () => {
+  const user = await AsyncStorage.getItem('user')
+  socket.emit('createUserSession', {
+    userId: JSON.parse(user)._id,
+    socketId: socket.id
+  })
 })
 
 export default socket
